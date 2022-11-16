cd556f54a0db23b42560c67ed2d3e25a9abc0adf
Jenerth Rodriguez <Connerjamestill@gmail.com>
2022-11-15 21:57:38 -0500

    Adding struct and csv file

--- dealership.c
+++ dealership.c
@@ -1,6 +1,14 @@
-#include "stdio.h"
-#include "string.h"
-#include "math.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct{
+    int vin;
+    int year;
+    char *model[30];
+    char *branch[30];
+}Car_t;
+
 
 int main(void)
 {
@@ -13,7 +21,14 @@ printf("Would you like to see our car catalog Enter Y for yes or N for no: \n");
 scanf("%c",&answer);
 
 if(answer == 'y' || answer == 'Y'){
-    printf("struct\n");
+    FILE *f;
+f = fopen("CarInventory.csv", "r");
+char *var;
+char varchar[301];
+var = &varchar[20];
+fscanf(f,"%s",var);
+printf("%s\n",var);
+fclose(f);
 }
 else {
     printf("Thank you for your time\n");
