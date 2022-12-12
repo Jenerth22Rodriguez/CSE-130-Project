ac2f52447f5c81f70b6766400d97d6f8e1716ceb
Jenerth Rodriguez <Connerjamestill@gmail.com>
2022-12-12 08:10:57 -0500

    Adding Formatting

--- dealership.c
+++ dealership.c
@@ -3,83 +3,91 @@
 #include <string.h>
 #include <math.h>
 
-typedef struct{
-    int vin;
-    int year;
-    char *model[30];
-    char *branch[30];
-}Car_t;
+void monthlyPayment(int *a, int *b)
+{
+   int downPayment;
+   int maxPayment;
 
-void monthlyPayment(int *a, int *b){
+   printf("Please enter the amount you plan to use as a down payment: ");
+   scanf("%d", &downPayment);
+   printf("Please enter your maximum monthly payment: ");
+   scanf("%d", &maxPayment);
 
-int downPayment;
-int maxPayment;
-
-printf("Please enter the amount you plan to use as a down payment: ");
-scanf("%d", &downPayment);
-printf("Please enter your maximum monthly payment: ");
-scanf("%d", &maxPayment);
-
-*a = *a - downPayment;
-while(1){
-if(*a / *b > maxPayment){
-    *b = *b + 1;      
-}
-else{
-*a = *a / *b;
-break;
-}
-}
+   *a = *a - downPayment;
+   while(1)
+   {
+      if(*a / *b > maxPayment)
+      {
+         *b = *b + 1;
+      }
+      else
+      {
+         *a = *a / *b;
+         break;
+      }
+   }
 }
 
 int main(void)
 {
-char answer;
-printf("**********************************************************************\n");
-printf("Hello welcome to \nAuto World first class \n");
-printf("Our goal is help you find the best auto for you\n");
-printf("Would you like to see our car catalog Enter Y for yes or N for no: ");
-scanf("%c",&answer);
-printf("**********************************************************************\n");
-int i = 0;
-if(answer == 'y' || answer == 'Y')
-{
-FILE *g;
-g = fopen("CarInventory.csv", "r");
-char line[10000];
+   // here I'm asking the user if the user want to continue
+   // if is it y is going to start the database if is it not
+   // the program is going to stop
 
-while(fgets(line,sizeof(line), g))
-{
-char *wordTaken;
-wordTaken = strtok(line, ",");
+   char answer;
+   printf("**********************************************************************\n");
+   printf("Hello welcome to \nAuto World first class \n");
+   printf("Our goal is help you find the best auto for you\n");
+   printf("Would you like to see our car catalog Enter Y for yes or N for no: ");
+   scanf("%c", &answer);
+   printf("**********************************************************************\n");
 
-while(wordTaken != NULL)
-{
-    printf("%-14s", wordTaken);
-    wordTaken = strtok(NULL, ",");
+   if(answer == 'y' || answer == 'Y')
+   {
+      FILE *g;
+      g = fopen("CarInventory.csv", "r");
+      char fileStore[10000];
+
+      while(fgets(fileStore, sizeof(fileStore), g))
+      {
+         char *wordTaken;
+
+         // The strtok function marks the end of the token by storing a null character
+
+         wordTaken = strtok(fileStore, ",");
+         // the strtok will separate the the file
+         //  The strtok function returns a pointer to the first character of the token.
+         //  If no token is found, a null pointer is returned.
+
+         while(wordTaken != NULL)
+         {
+            /*
+            Here I'm using the -%16 to print all the data inside the csv file with space
+            */
+            printf("%-16s", wordTaken);
+            wordTaken = strtok(NULL, ",");
+         }
+         printf("\n");
+      }
+      int carPrice;
+      char CarName[20];
+      char Model[20];
+
+      printf("Enter the car you like the most: ");
+      scanf("%s", CarName);
+      printf("Enter the model you like the most: ");
+      scanf("%s", Model);
+      printf("Enter the price of the car: ");
+      scanf("%d", &carPrice);
+      int months = 1;
+
+      monthlyPayment(&carPrice, &months);
+      printf("Your car payment will be %d per month for %d months.\n", carPrice, months);
+      printf("\nCONGRATULATIONS on your new %s %s!\n", CarName, Model);
+   }
+   else
+   {
+      printf("Thank you for your time\n");
+   }
+   return 0;
 }
-printf("\n");
-}
-int carPrice;
-char CarName[20];
-char Model[20];
-
-printf("Enter the car you like the most: ");
-scanf("%s",CarName);
-printf("Enter the model you like the most: ");
-scanf("%s", Model);
-printf("Enter the price of the car: ");
-scanf("%d",&carPrice);
-int months = 1;
-
-monthlyPayment(&carPrice,&months);
-printf("Your car payment will be %d per month for %d months.\n",carPrice,months);
-printf("\nCONGRATULATIONS on your new %s %s!\n",CarName, Model);
-}
-else 
-{
-    printf("Thank you for your time\n");
-}
-return 0;
-} 
-
